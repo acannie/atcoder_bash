c63158d016059bc923493414f9140991c42174d0
acannie <acannie0530@gmail.com>
2023-12-05 00:59:40 +0900

    code: mainとsolution1（枠組み）を実装

---
+++ templates/template.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function solution1 {
+    # -----------
+    #    設定
+    # -----------
+
+    # -----------
+    #    入力
+    # -----------
+
+    # -----------
+    #    計算
+    # -----------
+
+    echo "World"
+}
+
+function main {
+    solution1
+}
+
+main
