d3292211e97bf07284256331e70759efc233330b
acannie <acannie0530@gmail.com>
2023-12-05 01:00:36 +0900

    code: リストの要素をスペース区切りで出力する関数を実装

--- templates/template.sh
+++ templates/template.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# 定数
+YES="Yes"
+NO="No"
+
+# リストの要素をスペース区切りで出力
+function print_with_space {
+    local l=("$@")
+    l_len="${#l[@]}"
+    for i in "${!l[@]}"; do
+        element=${l[i]}
+        if [ ${i} -lt $((l_len - 1)) ]; then
+            echo -n "${element}"
+            echo -n " "
+        else
+            echo "${element}"
+        fi
+    done
+}
+
 function solution1 {
     # -----------
     #    設定
